708c4864fe09ee020c30cb76797a37b44a21be2c
brokol1 <50028968+brokol1@users.noreply.github.com>
2021-03-14 19:19:55 +0300

    2

--- index.js
+++ index.js
@@ -3,43 +3,60 @@ let stopBtn = document.querySelector(".btn-stop");
 let resetBtn = document.querySelector(".btn-reset");
 let clockface = document.getElementById("clockface");
 
-startBtn.addEventListener("click", function () {
-  startBtn.classList.toggle("btn-success");
-});
-
 let ms = 0,
   s1 = 0,
   s2 = 0,
   m1 = 0,
   m2 = 0,
   h1 = 0,
-  h2 = 0;
+  h2 = 0,
+  init = 0;
 
-setInterval(function () {
-  clockface.textContent = h2 + h1 + ":" + m2 + m1 + ":" + s2 + s1 + "." + ms;
-  ms++;
-  if (ms > 9) {
-    s1++;
-    ms = 0;
+function Tick() {
+  if (init == 1) {
+    let readout = h2 + h1 + ":" + m2 + m1 + ":" + s2 + s1 + "." + ms;
+    clockface.textContent = readout;
+    ms++;
+    if (ms > 9) {
+      s1++;
+      ms = 0;
+    }
+    if (s1 > 9) {
+      s2++;
+      s1 = 0;
+    }
+    if (s2 > 5) {
+      m1++;
+      s2 = 0;
+    }
+    if (m1 > 9) {
+      m2++;
+      m1 = 0;
+    }
+    if (m2 > 5) {
+      h1++;
+      m2 = 0;
+    }
+    if (h1 > 9) {
+      h2++;
+      m1 = 0;
+    }
+    let timerTick = setTimeout(function () {
+      Tick();
+      if (init == 0) {
+        clearTimeout(timerTick);
+      }
+    }, 100);
   }
-  if (s1 > 9) {
-    s2++;
-    s1 = 0;
+}
+//Функция запуска и остановки
+function StartStop() {
+  if (init == 0) {
+    init = 1;
+    Tick();
+  } else {
+    init = 0;
   }
-  if (s2 > 5) {
-    m1++;
-    s2 = 0;
-  }
-  if (m1 > 9) {
-    m2++;
-    m1 = 0;
-  }
-  if (m2 > 5) {
-    h1++;
-    m2 = 0;
-  }
-  if (h1 > 9) {
-    h2++;
-    m1 = 0;
-  }
-}, 10);
+}
+
+startBtn.addEventListener("click", StartStop);
