024678b8ca2082be29b299ad947d7fdfbfe03705
Nikolai <50028968+Brodatsky@users.noreply.github.com>
2022-11-29 15:53:44 +0300

    update

--- index.js
+++ index.js
@@ -1,142 +1,125 @@
-let startBtn = document.querySelector(".btn-start");
-let ol = document.getElementById("list");
-let resetBtn = document.querySelector(".btn-reset");
+let list = document.getElementById("list");
 let clockface = document.getElementById("clockface");
+let Tens = document.getElementById("tens");
+let Seconds = document.getElementById("seconds");
+let startBtn = document.querySelector(".btn-start");
+let resetBtn = document.querySelector(".btn-reset");
 let clearBtn = document.querySelector(".btn-clear");
 let saveBtn = document.querySelector(".btn-save");
+let Interval;
 
-let ms = 0,
-  s1 = 0,
-  s2 = 0,
-  m1 = 0,
-  m2 = 0,
-  h1 = 0,
-  h2 = 0,
-  init = 0,
-  readout = "0:00:00.0";
-clockface.textContent = readout;
-
-// Кнопка ресета
-resetBtn.addEventListener("click", () => {
-  (ms = 0),
-    (s1 = 0),
-    (s2 = 0),
-    (m1 = 0),
-    (m2 = 0),
-    (h1 = 0),
-    (h2 = 0),
-    (init = 0),
-    (readout = "0:00:00.0");
-  startBtn.textContent = "Start";
-  clockface.textContent = readout;
-});
-
-function Tick() {
-  if (init == 1) {
-    readout = h2 + h1 + ":" + m2 + m1 + ":" + s2 + s1 + "." + ms;
-    clockface.textContent = readout;
-    ms++;
-    if (ms > 9) {
-      s1++;
-      ms = 0;
-    }
-    if (s1 > 9) {
-      s2++;
-      s1 = 0;
-    }
-    if (s2 > 5) {
-      m1++;
-      s2 = 0;
-    }
-    if (m1 > 9) {
-      m2++;
-      m1 = 0;
-    }
-    if (m2 > 5) {
-      h1++;
-      m2 = 0;
-    }
-    if (h1 > 9) {
-      h2++;
-      m1 = 0;
-    }
-    let timerTick = setTimeout(function () {
-      Tick();
-      if (init == 0) {
-        clearTimeout(timerTick);
-      }
-    }, 100);
-  }
-}
+let tens = 00,
+  seconds = 00,
+  minutes = 00,
+  init = 0;
+// clockface.textContent = readout;
 
 //Функция запуска и остановки
 function StartStop() {
   if (init == 0) {
     init = 1;
     startBtn.textContent = `Stop`;
-    Tick();
+    clearInterval(Interval);
+    Interval = setInterval(startStopwatch, 10);
   } else {
     init = 0;
+    clearInterval(Interval);
     startBtn.textContent = `Start`;
-    createNewElement();
+    // createNewElement();
   }
 }
 
-function createNewElement() {
-  let li = document.createElement("li");
-  let txt = document.createElement("span");
+function startStopwatch() {
+  tens++;
 
-  txt.append(readout);
-
-  ol.appendChild(li).append(txt);
-}
-
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + d.toGMTString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
+  if (tens <= 9) {
+    Tens.innerHTML = "0" + tens;
   }
-  return "";
-}
 
-function deleteCookie(name) {
-  setCookie(name, "", {
-    "max-age": -1,
-  });
+  if (tens > 9) {
+    Tens.innerHTML = tens;
+  }
+
+  if (tens > 99) {
+    seconds++;
+    Seconds.innerHTML = "0" + seconds;
+    tens = 0;
+    Tens.innerHTML = "0" + 0;
+  }
+
+  if (seconds > 9) {
+    Seconds.innerHTML = seconds;
+  }
 }
 
 startBtn.addEventListener("click", () => {
   StartStop();
 });
 
-saveBtn.addEventListener("click", function () {
-  let listForCookie = ol.innerHTML;
-  setCookie("List", listForCookie, 1);
+// Кнопка ресета
+resetBtn.addEventListener("click", () => {
+  clearInterval(Interval);
+  tens = "00";
+  seconds = "00";
+  minutes = "00";
+  init = 0;
+  startBtn.textContent = "Start";
+
+  Tens.innerHTML = tens;
+  Seconds.innerHTML = tens;
 });
 
-clearBtn.addEventListener("click", function () {
-  ol.innerHTML = "";
-  deleteCookie("List");
-});
+// function createNewElement() {
+//   let li = document.createElement("li");
+//   let txt = document.createElement("span");
 
-function loadTodo() {
-  ol.innerHTML = getCookie("List");
-}
+//   txt.append(readout);
 
-loadTodo();
-console.log(loadTodo);
+//   ol.appendChild(li).append(txt);
+// }
+
+// function setCookie(cname, cvalue, exdays) {
+//   const d = new Date();
+//   d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+//   let expires = "expires=" + d.toGMTString();
+//   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+// }
+
+// function getCookie(cname) {
+//   let name = cname + "=";
+//   let decodedCookie = decodeURIComponent(document.cookie);
+//   let ca = decodedCookie.split(";");
+//   for (let i = 0; i < ca.length; i++) {
+//     let c = ca[i];
+//     while (c.charAt(0) == " ") {
+//       c = c.substring(1);
+//     }
+//     if (c.indexOf(name) == 0) {
+//       return c.substring(name.length, c.length);
+//     }
+//   }
+//   return "";
+// }
+
+// function deleteCookie(name) {
+//   setCookie(name, "", {
+//     "max-age": -1,
+//   });
+// }
+
+// saveBtn.addEventListener("click", function () {
+//   let listForCookie = list.innerHTML;
+//   setCookie("List", listForCookie, 1);
+// });
+
+// clearBtn.addEventListener("click", function () {
+//   list.innerHTML = "";
+//   deleteCookie("List");
+// });
+
+// function loadTodo() {
+//   list.innerHTML = getCookie("List");
+// }
+
+// loadTodo();
+// console.log(loadTodo);
